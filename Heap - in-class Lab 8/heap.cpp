469f54a85b64630e63ca264a0cb3d898d73de4b8
kaushalviraj <129000208+kaushalviraj@users.noreply.github.com>
2023-05-04 12:26:17 +0530

    Update heap.cpp

--- Heap - in-class Lab 8/heap.cpp
+++ Heap - in-class Lab 8/heap.cpp
@@ -4,49 +4,50 @@ using namespace std;
 // function to heapify the tree
 void heapify(int arr[], int n, int root)
 {
-   // build heapify
-   int left = 2 * root + 1;
-   int right = 2* root + 2;
+   int left = 2 * root + 1; // left child of the current root
+   int right = 2* root + 2;  // right child of the current root
    int largest;
 
+   // if the left child is larger than root
    if (left < n && arr[left]>arr[root] )
       largest = left;
    else
       largest = root;
+  
+   // if the right child is larger than current largest
    if (right<n && arr[right] > arr[largest])
       largest = right;
+  
+   // if the largest is not root
    if (largest != root)
    {
       int temp = arr[root];
-      swap(arr[root], arr[largest]);
-      //heapsize = heapsize - largest - 1;
-      heapify(arr,n-largest-1,largest);
+      swap(arr[root], arr[largest]);// swap the root with the largest child
+      heapify(arr,n-largest-1,largest);// recursively heapify the affected sub-tree
    }
-  
-   
-}
+} 
+
   
 // implementing heap sort
 void heapSort(int arr[], int n)
 {
    // build heap
    int heapsize = n;
+   // call heapify on each node to build a max heap
    for (int i = heapsize/2 ; i>=0; i--)
    {
       heapify(arr,heapsize,i);
    }
    
-  
    // extracting elements from heap one by one
-
    for (int i = n-1; i>=1;i--)
    {
-      swap(arr[i],arr[0]);
+      swap(arr[i],arr[0]); // swap the root with the last element
       heapsize--;
-      heapify(arr,heapsize,0);
+      heapify(arr,heapsize,0); // heapify the root
    }
-   
-}
+} 
+
   
 /* print contents of array */
 void displayArray(int arr[], int n)
